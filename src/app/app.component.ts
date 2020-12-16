cccb01aca27023a96fa9603e308777673b150492
mhd hasan alhallak <hasan_ak1996@hotmail.com>
2020-12-16 17:20:25 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { IDropdownSettings } from 'ng-multiselect-dropdown';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,95 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'example-angular';
+  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
+  registerForm!: FormGroup;
+  toppings = '';
+
+
+  items = [
+    {id: 1, name: 'Python'},
+    {id: 2, name: 'Node Js'},
+    {id: 3, name: 'Java'},
+    {id: 4, name: 'PHP', disabled: true},
+    {id: 5, name: 'Django'},
+    {id: 6, name: 'Angular'},
+    {id: 7, name: 'Vue'},
+    {id: 8, name: 'ReactJs'},
+  ];
+  selected = [
+    {id: 2, name: 'Node Js'},
+    {id: 8, name: 'ReactJs'}
+  ];
+
+
+
+
+  city = '';
+  cities: Array<string> = [];
+  selectedItem: Array<string> = [];
+  dropdownSettings: any = {};
+  closeDropdownSelection = false;
+  disabled = false;
+
+
+  options = [
+    'One',
+    'Two',
+    'Three'
+  ];
+
+  filteredOptions: Observable<string[]> | undefined;
+
+  constructor(
+
+    private formBuilder : FormBuilder
+    
+  ) { 
+      
+  }
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+
+      'myControl' :new FormControl(''), 
+    });
+
+    this.filteredOptions = this.registerForm.get('myControl')?.valueChanges.pipe(
+      startWith(''),
+      map(val => val =="@" ? this.options: [])
+    )
+
+    this.cities = ['Mumbai', 'New Delhi', 'Bangaluru', 'Pune', 'Navsari'];
+    this.dropdownSettings = {
+      singleSelection: false,
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      allowSearchFilter: true,
+      closeDropDownOnSelection: this.closeDropdownSelection
+    };
+    this.selectedItem = ['Mumbai'];
+
+  }
+  
+  filter(val: string): string[] {
+    return this.options.filter(option =>
+      option.toLowerCase().indexOf(val.toLowerCase()) === 1);
+  }
+
+  onItemSelect(item: any) {
+    console.log('onItemSelect', item);
+    console.log('selectedItem', this.selectedItem);
+  }
+
+  toggleCloseDropdownSelection() {
+    this.closeDropdownSelection = !this.closeDropdownSelection;
+    this.dropdownSettings = Object.assign({}, this.dropdownSettings, { closeDropDownOnSelection: this.closeDropdownSelection });
+  }
+
+   handleReset() {
+    this.selectedItem = [];
+  }
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { NgMultiSelectDropDownModule } from "ng-multiselect-dropdown";
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatInputModule} from '@angular/material/input';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgSelectModule,
+    NgMultiSelectDropDownModule.forRoot(),
+    MatAutocompleteModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
